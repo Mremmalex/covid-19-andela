798cf4e14240c58416306e5c35edd8e014b8ef68
mremmalex <mremmalex2@gmail.com>
2020-04-12 23:48:17 +0100

    first chellenge

--- src/estimator.py
+++ src/estimator.py
@@ -1,71 +1,50 @@
-
-#listing the varibles for the covid-19 assessment from Andala
-#region
-name = 'Africa'
-avgAge = 19.7
-avgDailyIncomeInUSD = 5
-avgDailyIncomePopulation = 0.71
-
-#other varibles to look out
-periodType = "days"
-timeToElapse = 58
-reportedCases = 674
-population = 66622705
-totalHospitalBeds = 1380614
-#starting the coding for the covid-19 assessment
-# declaring global varibles
-currentlyInfected  = reportedCases * 10
-severelyInfected = reportedCases * 50
-
-infectionByRequestedTime = currentlyInfected * 10242
-infectionByRequestedTime2 = severelyInfected * 10242
+#! /usr/bin/env python3.8.2
 
 #declaring function
 def estimator():
+        data = {
+        "region" : {
+            'name' : 'Africa',
+            'avgAge' : 19.7,
+            'avgDailyIncomeInUSD' : 5,
+            'avgDailyIncomePopulation' : 0.71
+        },
+        "periodType" : "days",
+        "timeToElapse":  58,
+        "reportedCases": 674,
+        "population" : 66622705,
+        "totalHospitalBeds" : 1380614
+        }
 
-    data = [
-        name,
-        avgAge,
-        avgDailyIncomeInUSD,
-        avgDailyIncomePopulation
+        currentlyInfected  = data["reportedCases"] * 10
+        severelyInfected = data["reportedCases"] * 50
 
-    ]
-    impact = [
-        currentlyInfected,
-        infectionByRequestedTime
-    ]
-    severeImpact = [
-        severelyInfected,
-        infectionByRequestedTime2
-    ]
-    
-    print('\ncomputing the data of the affected area')
-    print(f'name = {data[0]}')
-    print(f'avgAge = {data[1]}')
-    print(f'avgDailyIncomeInUSD = {data[2]}')
-    print(f'avgDailyIncomePopulation = {data[3]}')
+        def factor():
+            period = data['periodType']
+            multiplier = ''
 
-    print('\nestimating the impact level of the pendemic')
-    print(f'currentlyInfected = {impact[0]}')
-    print(f'infectionByRequestedTime = {impact[1]}')
-
-
-    print('\nestimating the severeImpact level of the pandemic')
-    print(f'severelyInfected = {severeImpact[0]}')
-    print(f'infectionByRequestedTime = {severeImpact[1]}')
+            if period == "days":
+                multiplier = 1;
+            elif period == "weeks":
+                multiplier = 7
+            elif period == "months":
+                multiplier = 30
+            return int( multiplier * data['timeToElapse'] / 3)
 
 
-estimator()
+        factor()
+        infectionByRequestedTime = currentlyInfected * 2**factor()
+        infectionByRequestedTime2 = severelyInfected * 2**factor()
 
+        data["impact"] = {
+                "currentlyInfected": currentlyInfected,
+                "infectionByRequestedTime": infectionByRequestedTime
+            }
 
+        data["severeImpact"] = {
+               "severelyInfected": severelyInfected,
+               "infectionByRequestedTime": infectionByRequestedTime2
+            }
+        return data
 
-# class estimator():
-#
-#     def __init__(self,reportedCases):
-#          self.reportedCases = reportedCases
-#
-#     def timator(self):
-#         print(self.reportedCases * 10)
-#
-# case = estimator(reportedCases)
-# case.timator()
+print(estimator())
